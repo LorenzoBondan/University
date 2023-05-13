dcca2629e5ecacd8ce0cb542794ba20765607fa3
LorenzoBondan <lbbondan@gmail.com>
2023-05-13 15:29:45 -0300

    courses page and course card

---
+++ frontend/src/components/CourseCard/index.tsx
@@ -0,0 +1,22 @@
+
+import { Course } from 'types';
+import './styles.css';
+
+type Props = {
+    course: Course;
+}
+
+const CourseCard = ({course} : Props) => {
+    return(
+        <div className='course-card-container base-card'>
+            <div className='course-card-top-container'>
+                <img src={course.imgUrl} alt="" />
+            </div>
+            <div className='course-card-bottom-container'>
+                <h4>{course.name}</h4>
+            </div>
+        </div>
+    );
+}
+
+export default CourseCard;
--- frontend/src/pages/Courses/index.tsx
+++ frontend/src/pages/Courses/index.tsx
@@ -1,10 +1,55 @@
 
+import { Course, SpringPage } from 'types';
 import './styles.css';
+import { useCallback, useEffect, useState } from 'react';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
+import CourseCard from 'components/CourseCard';
+import { Link } from 'react-router-dom';
 
 const Courses = () => {
+
+    const [page, setPage] = useState<SpringPage<Course>>();
+
+    const getCourses = useCallback(() => {
+        const params : AxiosRequestConfig = {
+          method:"GET",
+          url: "/courses",
+          params: {
+            page: 0,
+            size: 300
+          },
+        }
+    
+        requestBackend(params) 
+          .then(response => {
+            setPage(response.data);
+            window.scrollTo(0, 0);
+          })
+      }, [])
+  
+      useEffect(() => {
+        getCourses();
+      }, [getCourses]);
+
     return(
         <div className='courses-container'>
             <h1>Courses</h1>
+
+            <div className="row">
+                {page?.content
+                .sort((a,b) => a.name > b.name ? 1 : -1)
+                .map(course => (
+                    <div className="col-sm-6 col-lg-6 col-xl-3 courses-column" key={course.id}>
+                        <Link to={`/courses/${course.id}`}>
+                            <CourseCard course={course}/>
+                        </Link>
+                    </div>
+                    )
+                )
+                }
+            </div>
+
         </div>
     );
 }
