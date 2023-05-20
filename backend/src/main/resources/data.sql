fa86e3b48b4f6f4e8c86335e5f4873b1eca1db42
LorenzoBondan <lbbondan@gmail.com>
2023-05-20 10:21:01 -0300

    added more data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,10 +21,14 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (6, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (6, 2);
 
 INSERT INTO tb_course (name, img_Url, description) VALUES ('Software Engineering', 'https://cursos.unipampa.edu.br/cursos/engenhariadesoftware/files/2016/08/Logo-ES-Fundo-branco.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
-INSERT INTO tb_course (name, img_Url, description) VALUES ('Law', 'https://w7.pngwing.com/pngs/761/914/png-transparent-lady-justice-symbol-criminal-justice-balance-scales-miscellaneous-measuring-scales-sign.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
-INSERT INTO tb_course (name, img_Url, description) VALUES ('Medicine', 'https://cursos.unipampa.edu.br/cursos/engenhariadesoftware/files/2016/08/Logo-ES-Fundo-branco.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
-INSERT INTO tb_course (name, img_Url, description) VALUES ('Business', 'https://cursos.unipampa.edu.br/cursos/engenhariadesoftware/files/2016/08/Logo-ES-Fundo-branco.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
-
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Law', 'https://seeklogo.com/images/S/simbolo-de-direito-logo-52D04CB959-seeklogo.com.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Medicine', 'https://www.freepnglogos.com/uploads/medicine-logo-png-1.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Business', 'https://cdn.pixabay.com/photo/2016/03/19/04/40/brazilian-1266207_960_720.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Biomedicine', 'https://seeklogo.com/images/B/biomedicina-logo-9980D97C53-seeklogo.com.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Physical Education', 'https://seeklogo.com/images/E/educacao-fisica-logo-1EBC40DC9C-seeklogo.com.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Mechanical Engineering', 'https://seeklogo.com/images/S/S__mbolo_da_Engenharia_Mec__nica-logo-8CCBA4796F-seeklogo.com.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Civil Engineering', 'https://seeklogo.com/images/E/engenharia-civil-logo-98BEDE6119-seeklogo.com.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
+INSERT INTO tb_course (name, img_Url, description) VALUES ('Chemical Engineering', 'https://seeklogo.com/images/E/engenharia-quimica-logo-0AB6414414-seeklogo.com.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu erat justo. In ut urna diam. Aliquam a est sit amet eros lobortis accumsan. Curabitur a eleifend tortor. Vivamus nec congue erat. Nullam euismod sed felis in mollis. Vestibulum eu tempor metus, semper congue odio. Integer ipsum ex, ullamcorper a turpis sed, ullamcorper molestie massa. Donec eget urna iaculis, lobortis leo ut, cursus diam.');
 
 INSERT INTO tb_subject (name, semester) VALUES ('Programming I', 1);
 INSERT INTO tb_subject (name, semester) VALUES ('Web Development', 7);
@@ -32,7 +36,9 @@ INSERT INTO tb_subject (name, semester) VALUES ('Digital Law', 5);
 INSERT INTO tb_subject (name, semester) VALUES ('Criminal Law I', 2);
 INSERT INTO tb_subject (name, semester) VALUES ('Anatomy', 2);
 INSERT INTO tb_subject (name, semester) VALUES ('Business Intelligence', 4);
-
+INSERT INTO tb_subject (name, semester) VALUES ('Calculus I', 1);
+INSERT INTO tb_subject (name, semester) VALUES ('Calculus II', 2);
+INSERT INTO tb_subject (name, semester) VALUES ('Calculus III', 3);
 
 INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('ABC', 10, 1);
 INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('BCD', 20, 1);
@@ -41,6 +47,9 @@ INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('DEF', 40, 3)
 INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('EFG', 50, 4);
 INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('FGH', 50, 5);
 INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('GHI', 50, 6);
+INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('IJK', 50, 7);
+INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('JKL', 50, 8);
+INSERT INTO tb_class (code, limit_Of_Students, subject_id) VALUES ('KLM', 50, 9);
 
 INSERT INTO tb_user_course (user_id, course_id) VALUES (1,1);
 INSERT INTO tb_user_course (user_id, course_id) VALUES (4,2);
@@ -53,6 +62,21 @@ INSERT INTO tb_course_subject (course_id, subject_id) VALUES (2,3);
 INSERT INTO tb_course_subject (course_id, subject_id) VALUES (2,4);
 INSERT INTO tb_course_subject (course_id, subject_id) VALUES (3,5);
 INSERT INTO tb_course_subject (course_id, subject_id) VALUES (4,6);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (5,5);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (6,5);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (7,7);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (7,8);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (7,9);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (8,7);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (8,8);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (8,9);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (9,7);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (9,8);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (9,9);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (1,7);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (1,8);
+INSERT INTO tb_course_subject (course_id, subject_id) VALUES (1,9);
+
 
 INSERT INTO tb_class_student (class_id, student_id) VALUES (1,1);
 INSERT INTO tb_class_student (class_id, student_id) VALUES (3,1);
